58b8f03ffb96e06b40db7b1fe97fe7a1374047d8
Eric Son <ericsunghunson@gmail.com>
2020-11-09 02:21:41 -0800

    set up project files and used pg to connect node express project to docker container postgres db

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,12 +1,26 @@
 import logger from "./config/logger";
+import {Client} from "pg";
 
 export default class Database {
     public url: string;
     private dbName: string;
+    private client;
 
     constructor(url: string, name: string) {
         this.url = url;
         this.dbName = name;
+        this.client = new Client();
+        this.setUpConnection();
         logger.info("database");
+    }
+
+    private async setUpConnection(): Promise<void> {
+        const promise = this.client.connect();
+        promise.then(() => {
+            logger.info("The database has connected successfully")
+        }).catch((error) => {
+            logger.error(error)
+            logger.error("your database has not connected successfully")
+        });
     }
 }
