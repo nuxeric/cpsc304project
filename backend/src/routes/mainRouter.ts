d30297eb4b461c7348aa6d07cbd56fe30e054250
Peyton Seigo <pseigo@yahoo.com>
2020-11-09 03:41:54 -0700

    Add an index page to the root endpoint via the main router

--- backend/src/routes/mainRouter.ts
+++ backend/src/routes/mainRouter.ts
@@ -1,11 +1,10 @@
 import express, { Router } from "express";
-import testRoute from "./test/testRoute";
 import Database from "../database";
 import TestRoute from "./test/testRoute";
 
 export default class MainRouter {
     public router: Router;
-    private testRoute: testRoute;
+    private testRoute: TestRoute;
 
     constructor(db: Database) {
         this.router = express.Router();
@@ -14,6 +13,13 @@ export default class MainRouter {
     }
 
     private initializeRoutes(): void {
+        this.viewRoute();
         this.router.use("/test", this.testRoute.router);
+    }
+
+    private viewRoute(): void {
+        this.router.get("/", (req, res) => {
+            res.render('backend/page/index.ejs', { title: 'Warehouse Dashboard'} );
+        });
     }
 }
