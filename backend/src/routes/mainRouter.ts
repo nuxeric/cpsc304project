1037e832d83d02fd77a9e278c6e9c7f3a0cee9aa
Peyton Seigo <pseigo@yahoo.com>
2020-11-09 03:59:31 -0700

    Set up working endpoint for form demo

--- backend/src/routes/mainRouter.ts
+++ backend/src/routes/mainRouter.ts
@@ -1,20 +1,24 @@
 import express, { Router } from "express";
 import Database from "../database";
 import TestRoute from "./test/testRoute";
+import PersonnelRoute from "./personnel/personnelRoute";
 
 export default class MainRouter {
     public router: Router;
     private testRoute: TestRoute;
+    private personnelRoute: PersonnelRoute;
 
     constructor(db: Database) {
         this.router = express.Router();
         this.testRoute = new TestRoute(db);
+        this.personnelRoute = new PersonnelRoute(db);
         this.initializeRoutes();
     }
 
     private initializeRoutes(): void {
         this.viewRoute();
         this.router.use("/test", this.testRoute.router);
+        this.router.use("/personnel", this.personnelRoute.router);
     }
 
     private viewRoute(): void {
