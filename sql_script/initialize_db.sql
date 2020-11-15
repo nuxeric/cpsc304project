183138f1d1043a9ea33d11046d09e38a442d7535
ShaunDonn <shaunedonnelly@gmail.com>
2020-11-15 01:03:11 -0800

    Fixed formatting

--- sql_script/initialize_db.sql
+++ sql_script/initialize_db.sql
@@ -1,192 +1,264 @@
-CREATE TABLE personnel ( id INTEGER, birth_date DATE, first_name TEXT, last_name TEXT, PRIMARY KEY (id)
-);
-CREATE TABLE line_worker ( id INTEGER, responsibilities TEXT, PRIMARY KEY (id),
-   FOREIGN KEY (id)
-       REFERENCES personnel (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE
-);
-CREATE TABLE inventory_manager ( id INTEGER,
-PRIMARY KEY (id),
-FOREIGN KEY (id)
-       REFERENCES personnel (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE
-);
-CREATE TABLE personnel_manager (
-id INTEGER, number_of_employees_managed INTEGER, PRIMARY KEY (id),
-  FOREIGN KEY (id)
-  REFERENCES Personnel (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE
-);
-CREATE TABLE dimensions (
-  height DOUBLE PRECISION,
-  width DOUBLE PRECISION,
-  length DOUBLE PRECISION,
-  volume DOUBLE PRECISION,
-  PRIMARY KEY (height, width, length)
-);
-CREATE TABLE region( postal_code CHAR(6), municipality TEXT, province_or_territory TEXT, PRIMARY KEY (postal_code)
-);
-CREATE TABLE difference_cache( larger DOUBLE PRECISION,
-smaller DOUBLE PRECISION,
-difference INTEGER NOT NULL, PRIMARY KEY (larger, smaller)
+CREATE TABLE personnel ( 
+    id INTEGER, 
+    birth_date DATE, 
+    first_name TEXT, 
+    last_name TEXT, 
+    PRIMARY KEY (id)
 );
 
- CREATE TABLE warehouse (
-id INTEGER,
-total_volume DOUBLE PRECISION, occupied_volume DOUBLE PRECISION, street_address TEXT,
-postal_code CHAR(6), PRIMARY KEY (id), FOREIGN KEY (postal_code)
-REFERENCES region(postal_code) ON UPDATE CASCADE
-ON DELETE RESTRICT,
-FOREIGN KEY (total_volume, occupied_volume) REFERENCES difference_cache (larger, smaller) ON DELETE RESTRICT
-ON UPDATE RESTRICT
+CREATE TABLE line_worker ( 
+    id INTEGER, 
+    responsibilities TEXT, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (id)
+        REFERENCES personnel (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE inventory_manager ( id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id)
+        REFERENCES personnel (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+CREATE TABLE personnel_manager (
+    id INTEGER, 
+    number_of_employees_managed INTEGER, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (id)
+        REFERENCES Personnel (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE dimensions (
+    height DOUBLE PRECISION,
+    width DOUBLE PRECISION,
+    length DOUBLE PRECISION,
+    volume DOUBLE PRECISION,
+    PRIMARY KEY (height, width, length)
+);
+
+CREATE TABLE region( 
+    postal_code CHAR(6), 
+    municipality TEXT, 
+    province_or_territory TEXT, 
+    PRIMARY KEY (postal_code)
+);
+
+CREATE TABLE difference_cache( 
+    larger DOUBLE PRECISION,
+    smaller DOUBLE PRECISION,
+    difference INTEGER NOT NULL, 
+    PRIMARY KEY (larger, smaller)
+);
+
+CREATE TABLE warehouse (
+    id INTEGER,
+    total_volume DOUBLE PRECISION, 
+    occupied_volume DOUBLE PRECISION, 
+    street_address TEXT,
+    postal_code CHAR(6), 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (postal_code)
+        REFERENCES region(postal_code) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (total_volume, occupied_volume) 
+        REFERENCES difference_cache (larger, smaller) 
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
 );
 
 CREATE TABLE storage_container ( 
-  id INTEGER,
-warehouse_id INTEGER NOT NULL, 
-  occupied_volume DOUBLE PRECISION, 
-  height DOUBLE PRECISION,
-width DOUBLE PRECISION, 
-  length DOUBLE PRECISION, 
-  PRIMARY KEY (id),
-FOREIGN KEY (warehouse_id)
-       REFERENCES warehouse (id)
-       ON DELETE RESTRICT
-       ON UPDATE CASCADE,
-  FOREIGN KEY (height, width, length)
-       REFERENCES dimensions (height, width, length)
-       ON DELETE RESTRICT
-       ON UPDATE RESTRICT
+    id INTEGER,
+    warehouse_id INTEGER NOT NULL, 
+    occupied_volume DOUBLE PRECISION, 
+    height DOUBLE PRECISION,
+    width DOUBLE PRECISION, 
+    length DOUBLE PRECISION, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (warehouse_id)
+        REFERENCES warehouse (id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    FOREIGN KEY (height, width, length)
+        REFERENCES dimensions (height, width, length)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
 );
 
 CREATE TABLE producer (
-id INTEGER, street_address TEXT, postal_code CHAR(6), PRIMARY KEY (id), FOREIGN KEY (postal_code)
-REFERENCES region (postal_code) ON DELETE RESTRICT
-ON UPDATE CASCADE
+    id INTEGER, 
+    street_address TEXT, 
+    postal_code CHAR(6), 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (postal_code)
+        REFERENCES region (postal_code) 
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 
 CREATE TABLE inventory_type (
-
- type_name TEXT,
-height DOUBLE PRECISION,
-width DOUBLE PRECISION,
-length DOUBLE PRECISION,
-PRIMARY KEY (type_name),
-FOREIGN KEY (height, width, length)
-       REFERENCES Dimensions (height, width, length)
-       ON DELETE RESTRICT
-       ON UPDATE RESTRICT
+    type_name TEXT,
+    height DOUBLE PRECISION,
+    width DOUBLE PRECISION,
+    length DOUBLE PRECISION,
+    PRIMARY KEY (type_name),
+    FOREIGN KEY (height, width, length)
+        REFERENCES Dimensions (height, width, length)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
 );
 
 CREATE TABLE inventory ( 
-  serial_num INTEGER, 
-  container_id INTEGER NOT NULL, 
-  type_name TEXT NOT NULL, 
-  weight INTEGER, 
-  manufacture_date DATE, 
-  PRIMARY KEY (serial_num), 
-  FOREIGN KEY (container_id)
-	REFERENCES storage_container (id) 
-  	ON DELETE RESTRICT
-	ON UPDATE CASCADE,
-  FOREIGN KEY (type_name)
-	REFERENCES inventory_type (type_name) 
-  	ON DELETE RESTRICT
-	ON UPDATE CASCADE
+    serial_num INTEGER, 
+    container_id INTEGER NOT NULL, 
+    type_name TEXT NOT NULL, 
+    weight INTEGER, 
+    manufacture_date DATE, 
+    PRIMARY KEY (serial_num), 
+    FOREIGN KEY (container_id)
+	    REFERENCES storage_container (id) 
+  	    ON DELETE RESTRICT
+	    ON UPDATE CASCADE,
+    FOREIGN KEY (type_name)
+	    REFERENCES inventory_type (type_name) 
+  	    ON DELETE RESTRICT
+	    ON UPDATE CASCADE
 );
 
 CREATE TABLE tag (
-  name TEXT,
-  PRIMARY KEY (name)
+    name TEXT,
+    PRIMARY KEY (name)
 );
+
 CREATE TABLE categorizes (
-tag_name TEXT,
-inventory_type_name TEXT,
-PRIMARY KEY (tag_name, inventory_type_name), FOREIGN KEY (tag_name)
-       REFERENCES tag (name)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE,
-FOREIGN KEY (inventory_type_name) REFERENCES inventory_type (type_name) ON DELETE CASCADE
-ON UPDATE CASCADE
+    tag_name TEXT,
+    inventory_type_name TEXT,
+    PRIMARY KEY (tag_name, inventory_type_name), 
+    FOREIGN KEY (tag_name)
+        REFERENCES tag (name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (inventory_type_name) 
+        REFERENCES inventory_type (type_name) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
- CREATE TABLE delivery (
-  id INTEGER,
-  date DATE,
-  PRIMARY KEY (id)
+CREATE TABLE delivery (
+    id INTEGER,
+    date DATE,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE delivers_inventory( serial_num INTEGER,
-delivery_id INTEGER,
-PRIMARY KEY (serial_num, delivery_id), FOREIGN KEY (serial_num)
-REFERENCES inventory(serial_num) ON DELETE CASCADE
-ON UPDATE CASCADE,
-FOREIGN KEY (delivery_id) REFERENCES delivery (id) ON DELETE CASCADE
-ON UPDATE CASCADE
+CREATE TABLE delivers_inventory( 
+    serial_num INTEGER,
+    delivery_id INTEGER,
+    PRIMARY KEY (serial_num, delivery_id), 
+    FOREIGN KEY (serial_num)
+        REFERENCES inventory(serial_num) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (delivery_id) 
+        REFERENCES delivery (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
-CREATE TABLE production_facility ( id INTEGER,
-producer_id INTEGER NOT NULL, postal_code CHAR (6), street_address TEXT,
-PRIMARY KEY (id), FOREIGN KEY (postal_code)
-REFERENCES region (postal_code) ON DELETE RESTRICT
-ON UPDATE CASCADE,
-FOREIGN KEY (producer_id) REFERENCES producer (id) ON DELETE CASCADE
-ON UPDATE CASCADE
+
+CREATE TABLE production_facility ( 
+    id INTEGER,
+    producer_id INTEGER NOT NULL, 
+    postal_code CHAR (6), 
+    street_address TEXT,
+    PRIMARY KEY (id), 
+    FOREIGN KEY (postal_code)
+        REFERENCES region (postal_code) 
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    FOREIGN KEY (producer_id) 
+        REFERENCES producer (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
+
 CREATE TABLE incident (
 id INTEGER,
-producer_id INTEGER NOT NULL, date DATE,
-description TEXT, PRIMARY KEY (id), FOREIGN KEY (producer_id)
-       REFERENCES producer (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE
+producer_id INTEGER NOT NULL, 
+date DATE,
+description TEXT, 
+PRIMARY KEY (id), 
+FOREIGN KEY (producer_id)
+    REFERENCES producer (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-CREATE TABLE recipient ( id INTEGER, street_address TEXT, postal_code CHAR(6), PRIMARY KEY (id), FOREIGN KEY (postal_code)
-REFERENCES region (postal_code) ON DELETE RESTRICT
-ON UPDATE CASCADE
+CREATE TABLE recipient ( 
+    id INTEGER, 
+    street_address TEXT, 
+    postal_code CHAR(6), 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (postal_code)
+        REFERENCES region (postal_code) 
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
+
 CREATE TABLE dispatches (
-delivery_id INTEGER,
-recipient_id INTEGER,
-warehouse_id INTEGER,
-PRIMARY KEY (delivery_id, recipient_id, warehouse_id), FOREIGN KEY (delivery_id)
-       REFERENCES delivery (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE,
-FOREIGN KEY (recipient_id) REFERENCES recipient (id) ON DELETE CASCADE
-ON UPDATE CASCADE,
-FOREIGN KEY (warehouse_id) REFERENCES warehouse (id) ON DELETE CASCADE
-ON UPDATE CASCADE
+    delivery_id INTEGER,
+    recipient_id INTEGER,
+    warehouse_id INTEGER,
+    PRIMARY KEY (delivery_id, recipient_id, warehouse_id), 
+    FOREIGN KEY (delivery_id)
+        REFERENCES delivery (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (recipient_id) 
+        REFERENCES recipient (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (warehouse_id) 
+        REFERENCES warehouse (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE receives (
-delivery_id INTEGER,
-producer_id INTEGER,
-warehouse_id INTEGER,
-PRIMARY KEY (delivery_id, producer_id, warehouse_id), FOREIGN KEY (delivery_id)
-       REFERENCES delivery (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE,
-FOREIGN KEY (producer_id) REFERENCES recipient (id) ON DELETE CASCADE
-ON UPDATE CASCADE,
-FOREIGN KEY (warehouse_id) REFERENCES warehouse (id) ON DELETE CASCADE
-
-ON UPDATE CASCADE
+    delivery_id INTEGER,
+    producer_id INTEGER,
+    warehouse_id INTEGER,
+    PRIMARY KEY (delivery_id, producer_id, warehouse_id), 
+    FOREIGN KEY (delivery_id)
+        REFERENCES delivery (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (producer_id) 
+        REFERENCES recipient (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (warehouse_id) 
+        REFERENCES warehouse (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE works_in (
-pid INTEGER,
-warehouse_id INTEGER,
-PRIMARY KEY (pid, warehouse_id), FOREIGN KEY (pid)
-       REFERENCES personnel (id)
-       ON DELETE CASCADE
-       ON UPDATE CASCADE,
-FOREIGN KEY (warehouse_id) REFERENCES warehouse (id) ON DELETE CASCADE
-ON UPDATE CASCADE
+    pid INTEGER,
+    warehouse_id INTEGER,
+    PRIMARY KEY (pid, warehouse_id), 
+    FOREIGN KEY (pid)
+        REFERENCES personnel (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (warehouse_id) 
+        REFERENCES warehouse (id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 /*INSERT INITIAL DATA*/INSERT INTO personnel (id, birth_date, first_name, last_name)
@@ -203,27 +275,24 @@ VALUES (1, '1999-01-05', 'Eric', 'Son'),
     (10, '1999-08-11', 'Shaun', 'Donnelly'),
     (11, '2000-04-14', 'Peyton', 'Seigo');
     
-    INSERT
-INTO line_worker (id, responsibilities)
+INSERT INTO line_worker (id, responsibilities)
 VALUES (1, 'Mop and sweep the floors'),
     (2, 'Clean the machines'),
     (5, 'Fill hand sanitizers'),
     (9, 'Clean inventory boxes'),
     (10, 'Move inventory boxes');
     
-    INSERT
-INTO inventory_manager (id)
+INSERT INTO inventory_manager (id)
 VALUES (3),(4),(7),(8),(11);
 
-INSERT
-INTO personnel_manager (id, number_of_employees_managed)
+INSERT INTO personnel_manager (id, number_of_employees_managed)
 VALUES (1, 8),
     (2, 2),
     (5, 10),
     (6, 20),
     (10, 50);
   
-    INSERT INTO dimensions (height, width, length, volume)
+INSERT INTO dimensions (height, width, length, volume)
 VALUES (1, 10, 10, 100),
     (0.02, 0.02, 0.02, 0.000008),
     (0.5, 26, 39, 507),
@@ -323,24 +392,24 @@ VALUES ('pencil'),
     ('stationary');
 
 INSERT INTO delivery(id, date)
-    VALUES (1, '2020-10-23'),
-        (2, '2019-07-20'),
-        (3, '2018-03-22'),
-        (4, '2018-12-05'),
-        (5, '2019-09-15'),
-        (6, '2019-10-17'),
-        (7, '2020-02-18'),
-        (8, '2020-07-30'),
-        (9, '2020-09-02'),
-        (10, '2020-12-19');
+VALUES (1, '2020-10-23'),
+    (2, '2019-07-20'),
+    (3, '2018-03-22'),
+    (4, '2018-12-05'),
+    (5, '2019-09-15'),
+    (6, '2019-10-17'),
+    (7, '2020-02-18'),
+    (8, '2020-07-30'),
+    (9, '2020-09-02'),
+    (10, '2020-12-19');
        
 INSERT INTO delivers_inventory
-    VALUES (1, 3),
-        (2, 2),
-        (3, 1),
-        (4, 4),
-        (5, 5),
-        (6, 5);
+VALUES (1, 3),
+    (2, 2),
+    (3, 1),
+    (4, 4),
+    (5, 5),
+    (6, 5);
         
 INSERT INTO production_facility (id, producer_id, postal_code, street_address)
 VALUES (1, 1, 'V9M4N4', '1 Fairy St'), 
